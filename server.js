22aa275a339f307c086eefa2a633ff8d3b815dfd
Ol-create <oluola96@gmail.com>
2022-08-22 20:32:04 -0600

    feat: generate salt round with hash

--- server.js
+++ server.js
@@ -13,13 +13,16 @@ app.get("/users", (req, res)=>{
 
 app.post("/users", async (req, res) =>{
 	try{
-		const salt = await bcrypt.genSalt();
-		const hashedPassword = await bcrypt.hash(req.body.password, salt)
+		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		console.log("hash: ", hashedPassword)
+
+		const user = { name: req.body.name,
+			password: hashedPassword}
+			users.push(user);
+			res.status(201).send();
+	}catch{
+		res.status(500).send()
 	}
-const user = { name: req.body.name,
-password: req.body.password}
-users.push(user);
-res.status(201).send();
 })
 
 app.listen(3000);
