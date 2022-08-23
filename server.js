61d8d6ca81872bea9345be150197acdf285c5c71
Ol-create <oluola96@gmail.com>
2022-08-22 23:35:45 -0600

    Feat: add login route for authentication

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.get("/users", (req, res)=>{
 
 app.post("/users", async (req, res) =>{
 	try{
+		// const salt = await bcrypt.genSalt();
 		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		// console.log("Salt: ", salt)
 		console.log("hash: ", hashedPassword)
 
 		const user = { name: req.body.name,
@@ -25,4 +27,19 @@ app.post("/users", async (req, res) =>{
 	}
 })
 
+app.post("/users/login", async (req, res) => {
+	const user = users.find(user => user.name = req.body.name)
+	if(user == null) {
+		return res.status(400).send('Cannot find user')
+	}
+	try{
+     if (await bcrypt.compare(req.body.password, user.password)){
+		res.send("Success");
+	 }else{
+		res.send("Not Allowed")
+	 }
+	}catch{
+		res.status(500).send()
+	}
+})
 app.listen(3000);
